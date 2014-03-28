826cb925f1253f4b7f5ffc30b08210ff939b0dc9
fbird17 <ewccatch+github@gmail.com>
2014-03-28 13:14:26 -0500

    Seeing if new match works for Firefox

--- GM_AddFantasyToMyYahoo.user.js
+++ GM_AddFantasyToMyYahoo.user.js
@@ -3,8 +3,10 @@
 // @version         0.0.6
 // @namespace       https://github.com/fbird17
 // @description     Adds a Fantasy Baseball (and probably Football) link to the My Yahoo! homepage
-// @match           https://my.yahoo.com*
+// @match           https://my.yahoo.com
+// @match           https://my.yahoo.com/*
 // @include         https://my.yahoo.com*
+// @include         https://my.yahoo.com/*
 // @grant           GM_getValue
 // @grant           GM_setValue
 // @grant           GM_xmlhttpRequest
